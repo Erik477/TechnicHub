2824d8ba6464b8facfa011655dececf451e06c44
Erik477 <57523505+Erik477@users.noreply.github.com>
2021-11-17 15:56:55 +0100

    Klassen

---
+++ TechnicHub/Controllers/ChatforumController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechnicHub.Controllers
+{
+    public class ChatforumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
