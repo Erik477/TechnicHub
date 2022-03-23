e45a46ea83cafa4d8b7aaf506b44c611284575ee
Erik477 <57523505+Erik477@users.noreply.github.com>
2022-03-23 16:22:10 +0100

    async

--- TechnicHub/Models/DB/iRepositoryUsers.cs
+++ TechnicHub/Models/DB/iRepositoryUsers.cs
@@ -8,20 +8,20 @@ namespace TechnicHub.Models.DB
 {
     //TODO: asynchrone Programmierung
     // Vertrag (Interface) wurde erzeugt
-    interface iRepositoryUsers
+    interface IRepositoryUsers
     {
-        void Connect();
-        void Disconnect();
+        Task ConnectAsync();
+        Task DisconnectAsync();
 
         //CRUD-Operationen  (Create Read Update Delete)
-        bool Insert(Profile p);
-        bool Delete(int userId);
-        bool Update(int userId, Profile newUserData);
-        Profile GetUser(int userId);
-        List<Profile> GetAllUsers();
+        Task<bool> InsertAsync(Profile p);
+        Task<bool> DeleteAsync(int userId);
+        Task<bool> UpdateAsync(int userId, Profile newUserData);
+        Task<Profile> GetUserAsync(int userId);
+        Task<List<Profile>> GetAllUsersAsync();
 
         //weitere Methoden
 
-       bool Login(string username, string password);
+       Task<bool> LoginAsync(string username, string password);
     }
 }
