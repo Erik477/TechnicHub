f4ccbcd6ca6f1b9f4b7f3c61e1fa6d2520642bd6
Erik477 <57523505+Erik477@users.noreply.github.com>
2022-04-26 22:31:11 +0200

    Registration db sachen

--- TechnicHub/Models/DB/IRepositoryUsers.cs
+++ TechnicHub/Models/DB/IRepositoryUsers.cs
@@ -6,22 +6,19 @@ using TechnicHub.Models;
 
 namespace TechnicHub.Models.DB
 {
-    //TODO: asynchrone Programmierung
-    // Vertrag (Interface) wurde erzeugt
+
     interface IRepositoryUsers
     {
         Task ConnectAsync();
         Task DisconnectAsync();
-
-        //CRUD-Operationen  (Create Read Update Delete)
-        Task<bool> InsertAsync(Profile p);
+        Task<bool> InsertUserAsync(Profile user);
         Task<bool> DeleteAsync(int userId);
         Task<bool> UpdateAsync(int userId, Profile newUserData);
         Task<Profile> GetUserAsync(int userId);
         Task<List<Profile>> GetAllUsersAsync();
-
-        //weitere Methoden
-
-       Task<bool> LoginAsync(string username, string password);
+        Task<bool> InsertAsync(ProfileAndLanguages pl);
+        Task<bool> InsertLanguagesAsync(List<PLanguages> language, int user_id);
+        Task<int> GetUserIdAsync(string username);
+        Task<bool> LoginAsync(Profile p);
     }
 }
